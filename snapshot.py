a358c5a8a7e1995e6f9c51444c8d4ceb28f7c58e
brandon <brandon@gametaverse.io>
2024-05-28 16:36:47 +0800

    feat: snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -7,16 +7,16 @@ sys.path.append("../v3-polars/")
 from v3 import state
 
 # configurable 
-as_of = 19500000
+# as_of = 19907818
 
-if __name__ == "__main__":
+def queryDataWithBlock(block=19907818, update=False):
+    print("queryDataWithBlock", block, update)
     # initialize v3-polars
-    poolAddress = "0xbf7d01d6cddecb72c2369d1b421967098b10def7"
-    update = False
+    poolAddress = "0xBDB04e915B94FbFD6e8552ff7860E59Db7d4499a"
 
     pool = state.v3Pool(poolAddress, "ethereum", update=update)
 
-    swaps = pool.swaps
+    # swaps = pool.swaps
 
     # key = (from_address-tick_lower-tick_upper)
     # this is how core indexes positions but uses keccack(key) instead
@@ -33,10 +33,10 @@ if __name__ == "__main__":
                 )
         )
 
-    tick = pool.getTickAt(as_of)
+    tick = pool.getTickAt(block)
     lps = (
         mb.filter(
-            (pl.col("as_of") <= as_of)
+            (pl.col("as_of") <= block)
             &
             # positions are in range if tl <= tick < tu
             (pl.col("tick_lower") <= tick)
@@ -57,6 +57,28 @@ if __name__ == "__main__":
     ).is_empty(), "Negative liquidity"
 
     # we know that all lps are positive and in-range
-    print(lps)
-    lps.write_csv(f"lps_{as_of}.csv")
-    
+    print(block, update,"================================================")
+    # print(lps) 
+    # 初始化total
+    total_liquidity_delta = 0
+    for row in lps.iter_rows(named=True):
+        address = row['key'].split('-')[0]
+        liquidity_delta = row['liquidity_delta']
+        total_liquidity_delta += liquidity_delta
+        print(address, liquidity_delta)
+    print(total_liquidity_delta)
+    return
+
+
+if __name__ == "__main__":
+    # 解析单个参数
+    if len(sys.argv) > 1:
+        block = sys.argv[1]
+        update = sys.argv[2]
+        print("Parameter :", block, update)
+        queryDataWithBlock(block, update)
+    else:
+        print("No parameters provided.")
+        
+    # for block in range (19007818, 19907818):
+    #     queryDataWithBlock(block, False)
